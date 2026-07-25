f10afd981b8d1b32c9b66e4fd0ba99db86a9eb70
Desineni Gireesh <d.gireesh21@gmail.com>
2026-07-25 21:12:25 +0530

    fix(infra): update lambda source_dir path lambda/ -> lambdas/

--- Infrastructure/modules/project/main.tf
+++ Infrastructure/modules/project/main.tf
@@ -13,7 +13,7 @@ locals {
 module "lambda" {
   source        = "../lambda"
   function_name = "s3-cleanup-${var.environment}"
-  source_dir    = "${path.module}/../../../lambda/s3-cleanup"
+  source_dir    = "${path.module}/../../../lambdas/s3-cleanup"
   handler       = "handler.lambda_handler"
   runtime       = "python3.12"
   timeout       = var.lambda_timeout
